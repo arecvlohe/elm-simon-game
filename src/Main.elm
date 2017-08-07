3f295d6a4a3540deab0fd8146174a4794719141a
Adam Recvlohe <adam.recvlohe@gmail.com>
2017-08-07 16:45:39 -0400

    Minor cleanup

--- src/Main.elm
+++ src/Main.elm
@@ -2,7 +2,7 @@ module Main exposing (..)
 
 -- CORE IMPORTS
 
-import Html exposing (Html, program, text, div, button, input, audio)
+import Html exposing (Html, program, text, div, button, input, label)
 import Html.Attributes exposing (class, id, style, type_, checked, disabled)
 import Html.Events exposing (onClick, onCheck)
 import Random exposing (generate)
@@ -78,7 +78,7 @@ genAnimation count =
                 )
                 []
                 (range 0 <| count - 1)
-                ++ [ ( 0, SeqDone ) ]
+                ++ [ ( 300, SeqDone ) ]
                 |> (::) ( 0, SeqRun )
     in
         sequence <| withUnit millisecond seq
@@ -94,11 +94,15 @@ update msg model =
             if model.on then
                 model ! []
             else
-                { model | on = not model.on, count = model.count + 1 }
+                { model
+                    | on = not model.on
+                    , count = model.count + 1
+                }
                     ! [ generate NewSequence genSeq ]
 
         Reset ->
-            { initial | count = 1, on = True, strict = model.strict } ! [ generate NewSequence genSeq ]
+            { initial | count = 1, on = True, strict = model.strict }
+                ! [ generate NewSequence genSeq ]
 
         ResetColor ->
             { model | clicked = "" } ! []
@@ -121,17 +125,39 @@ update msg model =
                 if model.isRunning then
                     model ! []
                 else if checksPass && length nextUserInput == model.count then
-                    { model | userInput = [], clicked = color, count = model.count + 1 }
-                        ! [ playsound color, after 300 millisecond ResetColor, genAnimation <| model.count + 1 ]
+                    { model
+                        | userInput = []
+                        , clicked = color
+                        , count = model.count + 1
+                    }
+                        ! [ playsound color
+                          , after 300 millisecond ResetColor
+                          , genAnimation <| model.count + 1
+                          ]
                 else if checksPass then
-                    { model | userInput = nextUserInput, clicked = color }
-                        ! [ playsound color, after 300 millisecond ResetColor ]
+                    { model
+                        | userInput = nextUserInput
+                        , clicked = color
+                    }
+                        ! [ playsound color
+                          , after 300 millisecond ResetColor
+                          ]
                 else if not checksPass && model.strict then
-                    { initial | count = 1, on = True, sequence = model.sequence, strict = True }
-                        ! [ playsound color, genAnimation 1 ]
+                    { initial
+                        | count = 1
+                        , on = True
+                        , sequence = model.sequence
+                        , strict = True
+                    }
+                        ! [ playsound color
+                          , genAnimation 1
+                          ]
                 else
                     { model | clicked = color, userInput = [] }
-                        ! [ playsound color, after 300 millisecond ResetColor, genAnimation model.count ]
+                        ! [ playsound color
+                          , after 300 millisecond ResetColor
+                          , genAnimation model.count
+                          ]
 
         StrictMode bool ->
             if bool then
@@ -197,7 +223,10 @@ view { count, clicked, strict, isRunning } =
             ]
         , div []
             [ div [] [ text ("Count " ++ (toString count)) ]
-            , input [ type_ "checkbox", checked strict, onCheck StrictMode, disabled isRunning ] []
+            , div []
+                [ label [] [ text "Strict" ]
+                , input [ type_ "checkbox", checked strict, onCheck StrictMode, disabled isRunning ] []
+                ]
             , button [ onClick Start, disabled isRunning ] [ text "Start" ]
             , button [ onClick Reset, disabled isRunning ] [ text "Reset" ]
             ]
